a75c3704c4ea3024cd146ec8944fdd3c16ce2dec
Corina Wong <hi@corinawong.com>
2017-10-04 16:26:16 +0000

    Counting characters

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+function countLetters(string) {
+  var letterCount = {};
+  string = string.replace(/ /g, '');
+  for (var i = 0; i < string.length; i++) {
+    var letter = string[i];
+    if (letter in letterCount) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+}
+
+console.log(countLetters("lighthouse in the house"));
